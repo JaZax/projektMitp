4641d071b3d5e07ec31f0790f98e9e18c480b1d4
JaZa <47424360+JaZax@users.noreply.github.com>
2024-11-05 10:24:37 +0100

    New images

--- translateAtts.py
+++ translateAtts.py
@@ -11,6 +11,35 @@ nameTranslate = {
     "bodyRomantic": "Romantyk",
     "accessoryRomantic": "Romantyk",
 
+    "hatBusiness": "Biznesmen",
+    "bodyBusiness": "Biznesmen",
+    "accessoryBusiness": "Biznesmen",
+
+    "hatRockman": "Rockman",
+    "bodyRockman": "Rockman",
+    "accessoryRockman": "Rockman",
+
+    "hatGoth": "Goth",
+    "bodyGoth": "Goth",
+    "accessoryGoth": "Goth",
+
+    "hatSummer": "Wakacje",
+    "bodySummer": "Wakacje",
+    "accessorySummer": "Wakacje",
+
+    "hatWiking": "Wiking",
+    "bodyWiking": "Wiking",
+    "accessoryWiking": "Wiking",
+
+    "hatHero": "Bohater",
+    "bodyHero": "Bohater",
+    "accessoryHero": "Bohater",
+
+    "hatPrison": "Więźniara",
+    "bodyPrison": "Więźniara",
+    "accessoryPrison": "Więźniara",
+
     "frontBg1": "Niebo",
     "frontBg2": "Ogień",
+    "frontBg3": "Miasto",
 }
